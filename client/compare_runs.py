a82f1a89ba046c7901b896e495c081d8254ef263
Sarah Abdelazim <sarah_s96@hotmail.com>
2025-03-25 22:51:50 -0500

    Update compare_runs.py

--- client/compare_runs.py
+++ client/compare_runs.py
@@ -7,10 +7,9 @@ def get_baseline_run(entity='hamelsmu', project='my-report-project', tag='baseli
     "Get the baseline run from the project using tags"
 
     api = wandb.Api()
-    runs=api.runs(f'{entity}/{project}', {"tags": {"$in": [tag]}})
+    runs = api.runs(f'{entity}/{project}', {"tags": {"$in": [tag]}})
     assert len(runs) == 1, 'There must be exactly one run with the tag "baseline"'
     return runs[0]
-
 
 def compare_runs(entity='hamelsmu',
                  project='cicd_demo',
@@ -26,21 +25,28 @@ def compare_runs(entity='hamelsmu',
     assert run_id, 'You must set the RUN_ID environment variable or pass a `run_id` argument'
 
     baseline = get_baseline_run(entity=entity, project=project, tag=tag)
+
+    # Create the report
     report = wr.Report(entity=entity, project=project,
                        title='Compare Runs',
                        description=f"A comparison of runs, the baseline run name is {baseline.name}") 
 
+    # Create a panel with the run comparison
     pg = wr.PanelGrid(
-        runsets=[wr.Runset(entity, project, "Run Comparison").set_filters_with_python_expr(f"ID in ['{run_id}', '{baseline.id}']")],
-        panels=[wr.RunComparer(diff_only='split', layout={'w': 24, 'h': 15}),]
+        runsets=[wr.Runset(entity, project, "Run Comparison").set_filters(
+            f"ID == '{run_id}' or ID == '{baseline.id}'")],
+        panels=[wr.RunComparer(diff_only='split', layout={'w': 24, 'h': 15})]
     )
+
+    # Add the panel to the report
     report.blocks = report.blocks[:1] + [pg] + report.blocks[1:]
     report.save()
 
-    if os.getenv('CI'): # is set to `true` in GitHub Actions https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-        with open(os.environ['GITHUB_OUTPUT'], 'a') as f: # write the output variable REPORT_URL to the GITHUB_OUTPUT file
+    if os.getenv('CI'):  # is set to `true` in GitHub Actions
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as f:  # write the output variable REPORT_URL to the GITHUB_OUTPUT file
             print(f'REPORT_URL={report.url}', file=f)
+    
     return report.url
 
 if __name__ == '__main__':
-    print(f'The comparison report can found at: {compare_runs()}')
+    print(f'The comparison report can be found at: {compare_runs()}')
